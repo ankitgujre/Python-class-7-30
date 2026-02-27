b407ef6a5ce77fc7c3ee4f39e83bad9149e2e6b5
ankitgujre <ankitgujre374@gmail.com>
2026-02-27 19:43:16 +0530

    set data type

---
+++ SetDataType.py
@@ -0,0 +1,10 @@
+# Change item
+'''
+Once a set is created you cannot change its item but you can add new items.
+
+Python add set item
+to add one item to a set use the add() method
+'''
+s = {1,2,3}
+s.add(50)
+print(s)
